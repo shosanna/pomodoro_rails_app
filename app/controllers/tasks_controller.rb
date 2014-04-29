4082c116eeee0eac8bb457f1f8342a26aa2efbf2
Jakub Arnold <darthdeus@gmail.com>
2014-04-29 23:20:22 +0200

    Fix done & active

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,19 +8,22 @@ class TasksController < AuthenticatedController
     end
   end
 
-
   def create
-    @task = current_user.tasks.build(params.require(:task).permit(:content))
-    @task.save!
+    task = current_user.tasks.build(params.require(:task).permit(:content))
+    task.save!
     @tasks = Task.all
   end
 
   def destroy
-
+    task = current_user.tasks.find(params[:id])
+    task.destroy
+    @tasks = Task.all
   end
 
-  def show
-
+  def update
+    task = current_user.tasks.find(params[:id])
+    task.update_attributes!(done: (params[:done] == "true"))
+    @tasks = Task.all
   end
 
   def start
